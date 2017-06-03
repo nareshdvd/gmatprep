3526f420be81c871e16e5d130224f37c062eaccd
nareshdvd <naresh.dwivedi1987@gmail.com>
2017-06-03 15:09:52 +0000

    fixed issues with payment testing

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,5 +1,6 @@
 class PaymentsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:paypal_callback]
+  skip_before_action :verify_authenticity_token, only: [:notification]
   def paypal_callback
     # plan_id = params[:item_number]
     # paypal_txn_id = params[:tx]
--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -3,8 +3,8 @@ class PlansController < ApplicationController
     respond_to do |format|
       if (subscriptions = current_user.subscriptions.with_plan.not_free.not_elapsed).blank?
         @plan = Plan.find_by_id(params[:id])
-        subscription = current_user.subscriptions.create(plan_id: plan.id, is_active: false)
-        @payment = subscription.payments.create(status: Payment::STATUS[:pending], amount: plan.amount, currency: plan.currency, gm_txn_id: SecureRandom.hex(8))
+        subscription = current_user.subscriptions.create(plan_id: @plan.id, is_active: false)
+        @payment = subscription.payments.create(status: Payment::STATUS[:pending], amount: @plan.amount, currency: @plan.currency, gm_txn_id: SecureRandom.hex(8))
         format.js {render "plans/init_subscribe"}
       else
         subscription = subscriptions.with_payments.where("payments.status = ?", Payment::STATUS[:pending]).first
