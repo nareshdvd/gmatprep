7f98b75804795ea838e8d92dc7d10d590cc4a2d2
nareshd <nareshd@uvisionconsulting.com>
2017-06-10 04:03:50 +0530

    fixed query

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -33,7 +33,7 @@ class Subscription < ActiveRecord::Base
 
   "papers.start_time IS NULL OR DATE_ADD(papers.start_time, INTERVAL #{Paper::MINUTES} MINUTE) > NOW()"
   scope :not_exhausted, -> {
-    joins("LEFT OUTER JOIN papers ON papers.subscription_id=subscriptions.id").select("SUM(papers.start_time IS NULL OR DATE_ADD(papers.start_time, INTERVAL #{Paper::MINUTES} MINUTE) > NOW()) as unfinished_count, COUNT(papers.id) as p_count, subscriptions.id as sub_id, plans.paper_count as paper_count, subscriptions.*").group("sub_id").having("(p_count < paper_count) OR (unfinished_count = 1)")
+    joins("LEFT OUTER JOIN papers ON papers.subscription_id=subscriptions.id").select("SUM((papers.finish_time IS NULL) AND (papers.start_time IS NULL OR DATE_ADD(papers.start_time, INTERVAL #{Paper::MINUTES} MINUTE) > NOW())) as unfinished_count, COUNT(papers.id) as p_count, subscriptions.id as sub_id, plans.paper_count as paper_count, subscriptions.*").group("sub_id").having("(p_count < paper_count) OR (unfinished_count = 1)")
   }
 
   scope :with_payments, -> {
