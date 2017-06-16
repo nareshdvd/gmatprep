546700b8f36909643cb939902aa1e8a440c5316a
nareshd <nareshd@uvisionconsulting.com>
2017-06-16 19:59:09 +0530

    fixed css

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,4 +1,13 @@
 class ReportsController < ActionController::Base
   def index
+    paper_id = params[:paper_id]
+    @paper = Paper.find_by(id: paper_id)
+    respond_to do |format|
+      if @paper.subscription.user_id != current_user.id
+        format.html{redirect_to "404NotFound"}
+      else
+        format.html
+      end
+    end
   end
 end
