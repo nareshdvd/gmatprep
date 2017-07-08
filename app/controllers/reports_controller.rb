789a38ecbc6d0fe1c798ed3f14d6d5ac167c2756
nareshd <nareshd@uvisionconsulting.com>
2017-07-08 11:12:18 +0530

    minor fixes

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,7 +3,9 @@ class ReportsController < ApplicationController
     paper_id = params[:paper_id]
     @paper = Paper.find_by(id: paper_id)
     respond_to do |format|
-      if @paper.subscription.user_id != current_user.id
+      if @paper.papers_questions.count != 41 || @paper.papers_questions.last.unanswered?
+        format.html{redirect_to root_path, notice: "This paper hasn't been finished, so report generation is not possible"}
+      elsif @paper.subscription.user_id != current_user.id
         format.html{redirect_to "404NotFound"}
       else
         format.html
@@ -15,7 +17,9 @@ class ReportsController < ApplicationController
     paper_id = params[:paper_id]
     @paper = Paper.find_by(id: paper_id)
     respond_to do |format|
-      if @paper.blank?
+      if @paper.papers_questions.count != 41 || @paper.papers_questions.last.unanswered?
+        format.html{redirect_to root_path, notice: "This paper hasn't been finished, so report generation is not possible"}
+      elsif @paper.blank?
         format.html{redirect_to root_path, notice: "Report Not Found"}
       else
         if @paper.subscription.user_id != current_user.id
