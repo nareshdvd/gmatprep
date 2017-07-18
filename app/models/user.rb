63235a88d3cb71d4cde761884abd0e08e1725498
nareshd <nareshd@uvisionconsulting.com>
2017-07-18 14:31:05 +0530

    minor fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -139,7 +139,7 @@ class User < ActiveRecord::Base
       payment = invoice.payments.preload(:payment_methods).joins(:payment_methods).where("payment_methods.name=? AND payments.status=?", payment_params[:payment_method], Payment::STATUS[:pending]).first
       payment, payment_method = invoice.create_pending_payment(payment_params) if payment.blank?
       if payment_method.blank?
-        payment.get_or_add_payment_method(payment_params[:payment_method], payment_params.except(:payment_method))
+        payment_method = payment.get_or_add_payment_method(payment_params[:payment_method], payment_params.except(:payment_method))
       end
       #payment_method = payment.payment_methods.detect{|payment_method| payment_method.name == payment_params[:payment_method]}
     end
