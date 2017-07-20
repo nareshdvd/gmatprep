3737252be2bf4b7964e24a385df9ee2f4cf12f0d
nareshd <nareshd@uvisionconsulting.com>
2017-07-20 18:40:31 +0530

    fixed footer css and added overview html page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-  skip_before_filter :authenticate_user!, only: [:index, :contact_us, :disclaimer]
+  skip_before_filter :authenticate_user!, only: [:index, :contact_us, :disclaimer, :overview]
   def index
     monitor = InfluxMonitor.should_monitor?(cookies, :asynchronous_visitor_monitoring)
     if current_user.blank?
@@ -53,6 +53,12 @@ class HomeController < ApplicationController
     end
   end
   def disclaimer
+    respond_to do |format|
+      format.html
+    end
+  end
+
+  def overview
     respond_to do |format|
       format.html
     end
